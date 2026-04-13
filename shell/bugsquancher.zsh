4a47096dcffa6a76d51c271a4b65957947685c73
Hisham <hisham.khashman@gmail.com>
2026-04-13 23:33:54 +0900

    first version for bugsquancher.zsh

--- shell/bugsquancher.zsh
+++ shell/bugsquancher.zsh
@@ -1,12 +1,20 @@
 # Capture Output of terminal (commands, output and exit status) detect failure and pass data to Python
-# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/capture maybe useful? check later
+
 
 # hook into zsh lifecycle
 
-# detect failed command
+  # detect failed command
+  preexec() {
+    LAST_COMMAND="$1" # $1 is the command about to be executed
+  }
+  # capture output
+  precmd() {
+    EXIT_CODE=$?
+    if ((EXIT_CODE >0)); then
+      # call python with variables
+      python3 /path/to/bugsquancher.py "$LAST_COMMAND" "$EXIT_CODE"
+      fi
+  }
 
-# capture output
-
-# call python
 
 # print appended output
