ade2d3a571a28f5d2e0714412801efe6ffc49031
Hisham <hisham.khashman@gmail.com>
2026-04-15 01:07:28 +0900

    added a match_pattern in matcher.py

--- bugsquancher/matcher.py
+++ bugsquancher/matcher.py
@@ -4,6 +4,13 @@
 
 # load patterns
 
+from patterns import patterns
 # compare normalized text against keywords or regex
 
 # return matched pattern or None
+
+def match_pattern(cleaned_output):
+    for pattern in patterns:
+        if pattern.matches(cleaned_output):
+            return pattern
+    return None
