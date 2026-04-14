2dbb16400d02b4403a0db74f73664c52c879aa90
Hisham <hisham.khashman@gmail.com>
2026-04-14 16:57:50 +0900

    added explanation to connection between zsh and main.py

--- bugsquancher/main.py
+++ bugsquancher/main.py
@@ -5,6 +5,13 @@ import sys
 # read the passed output in bugsquancher.zsh
     # sys.argv --> metadata (command is sys.argv[1], exit code is sys.argv[2])
     # sys.stdin --> actual error output sys.stdin.read()
+    #
+    # Explantion: passes values as positional arguments
+    # python3 "$APP_PATH" "$LAST_COMMAND" "$EXIT_CODE"
+    # sys.argv[0] = "main.py"
+    # sys.argv[1] = LAST_COMMAND
+    # sys.argv[2] = EXIT_CODE
+
 command = sys.argv[1]
 exit_code = sys.argv[2]
 output = sys.stdin.read()
